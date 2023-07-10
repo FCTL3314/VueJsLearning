4b9b3be7af7265f70c30bee8b3df3b8ecd6c1da6
Nikita Solouyou <solovev.nikita.05@gmail.com>
2023-07-10 15:03:07 +0300

    First component

---
+++ initial/src/components/MyComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="root">
+    <label>
+      Выберите кол-во продуктов:
+      <button type="button" v-on:click="increaseNumber">+</button>
+      <input type="number" min="1" max="10" :value="number">
+      <button type="button" v-on:click="decreaseNumber">-</button>
+    </label>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyComponent',
+    data() {
+      return {
+        number: 1,
+      }
+    },
+    methods: {
+      increaseNumber() {
+        if (this.number < 10) {
+          this.number += 1
+        }
+      },
+      decreaseNumber() {
+        if (this.number > 1) {
+          this.number -= 1
+        }
+      },
+    }
+  }
+</script>
